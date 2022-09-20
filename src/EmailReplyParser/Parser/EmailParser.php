6e9e8fa498428335c5a802f6b6d1a83b8df50599
Mark Kremer <markkremer@pop-os.localdomain>
2022-09-20 12:56:18 +0200

    Allow multiple signature regexes & add Dutch send from my iPhone signature

--- src/EmailReplyParser/Parser/EmailParser.php
+++ src/EmailReplyParser/Parser/EmailParser.php
@@ -25,7 +25,10 @@ class EmailParser
      *
      * @var string
      */
-    private $signatureRegex = '/(?:^\s*--|^\s*__|^-\w|^-- $)|(?:^Sent from (my|Mail) (?:\s*\w+){1,4}$)|(?:^={30,}$)$/s';
+    private $signatureRegex = [
+        '/(?:^\s*--|^\s*__|^-\w|^-- $)|(?:^Sent from (my|Mail) (?:\s*\w+){1,4}$)|(?:^={30,}$)$/s',
+        '/(?:^\s*--|^\s*__|^-\w|^-- $)|(?:^Verstuurd vanaf mijn (?:\s*\w+){1,4}$)|(?:^={30,}$)$/s',
+    ];
 
     /**
      * @var string[]
@@ -152,7 +155,7 @@ class EmailParser
     }
 
     /**
-     * @return string
+     * @return string[]
      * @since 2.7.0
      */
     public function getSignatureRegex()
@@ -161,12 +164,12 @@ class EmailParser
     }
 
     /**
-     * @param string $signatureRegex
+     * @param string[] $signatureRegex
      *
      * @return EmailParser
      * @since 2.7.0
      */
-    public function setSignatureRegex($signatureRegex)
+    public function setSignatureRegex(array $signatureRegex)
     {
         $this->signatureRegex = $signatureRegex;
 
@@ -206,7 +209,13 @@ class EmailParser
 
     private function isSignature($line)
     {
-        return preg_match($this->signatureRegex, $line) ? true : false;
+        foreach ($this->signatureRegex as $regex) {
+            if (preg_match($regex, $line)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
