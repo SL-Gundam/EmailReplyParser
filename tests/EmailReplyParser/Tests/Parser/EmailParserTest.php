48c922d2665c77c47b6dd8f2f30edd15658e4b2e
SL-Gundam <SL-Gundam@users.noreply.github.com>
2026-09-17 21:45:23 +0200

    Trivial test case correction coding style

--- tests/EmailReplyParser/Tests/Parser/EmailParserTest.php
+++ tests/EmailReplyParser/Tests/Parser/EmailParserTest.php
@@ -455,10 +455,10 @@ merci d'avance", $email->getVisibleText());
      */
     public function testCustomSignatureRegex()
     {
-        $signatureRegex = [
+        $signatureRegex = array(
             '/^DOESNT_MATCH_ANYTHING$/',
             '/(?:^\s*--|^\s*__|^-- $)|(?:^Sent from my (?:\s*\w+){1,3})$/s',
-        ];
+        );
         $this->parser->setSignatureRegex($signatureRegex);
         $email = $this->parser->parse($this->getFixtures('email_ls-l.txt'));
         $fragments = $email->getFragments();
